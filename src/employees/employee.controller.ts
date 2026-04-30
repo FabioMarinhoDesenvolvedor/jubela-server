2ef7b492ea1fb03867f059d1fc87d0cd72d85c58
lucasNasc <lucasfortunato328@gmail.com>
2026-04-30 18:34:23 -0300

    rota employees

--- src/employees/employee.controller.ts
+++ src/employees/employee.controller.ts
@@ -10,6 +10,7 @@ import {
   UsePipes,
 } from '@nestjs/common';
 import { SkipThrottle } from '@nestjs/throttler';
+import { Public } from 'src/auth/decorators/set-metadata.decorator';
 import { SetRoutePolicy } from 'src/auth/decorators/set-route-policy.decorator';
 import { TokenPayloadDTO } from 'src/auth/dto/token-payload.dto';
 import { RoutePolicyGuard } from 'src/auth/guards/route-policy.guard';
@@ -31,9 +32,10 @@ import { EmployeesService } from './employee.service';
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @Public()
   @SkipThrottle({ read: true, auth: true, refresh: true, preference: true })
   @Post()
-  @SetRoutePolicy(EmployeeRole.ADMIN)
+  // @SetRoutePolicy(EmployeeRole.ADMIN)
   @UsePipes(ReqBodyCpfValidation, ReqBodyPhoneNumberValidation)
   Create(@Body() body: CreateEmployeeDTO) {
     return this.employeesService.Create(body);
